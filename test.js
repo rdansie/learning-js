e64036d04f0c302ebee33f1c87db1c54d1f94912
Ryan Dansie <8863287+rdansie@users.noreply.github.com>
2022-12-13 21:45:59 +0100

    completed loops1 exercise

--- test.js
+++ test.js
@@ -1,11 +1,33 @@
-let ask = (question, yes, no) => {
-    if (confirm(question)) {
-        yes();
-    }else no();
-  }
-  
-  ask(
-    "Do you agree?",
-    function() { alert("You agreed."); },
-    function() { alert("You canceled the execution."); }
-  );
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+
+//Iterate through people Array
+
+for (let tits of people) {
+    //if name is equal to Phil or Lola concatenate to refused paragraph
+    if (tits === "Phil" || tits === "Lola") {
+        refused.textContent += tits + ", ";
+    } else {
+        //else concatenate to admitted paragraph
+        admitted.textContent += tits + ", ";
+    }
+}
+
+refused = refused.slice(0, -1);
+admitted = admitted.slice(0, -1);
+
+    
+
+
+
+
+    //else concatenate to admitted paragraph
+
+
+// refused.textContent += ;
+// admitted.textContent += ;
